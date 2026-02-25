5ea8d51ae4da061d960f23e0dea16d71b12ffc68
krocher <kili.rocher@orange.fr>
2026-02-25 14:05:50 +0100

    Logging marginal fitnesses + Adding their R plots

--- Data_analysis_inversions.R
+++ Data_analysis_inversions.R
@@ -2,19 +2,23 @@ rm(list = ls())
 library(dplyr)
 library()
 
-## File = "/home/taubier/Documents/SAUVEGARDES_STAGIAIRES/Kilian/Results/freq_ 90%Inv .csv"
+ # File = "/home/taubier/Documents/SAUVEGARDES_STAGIAIRES/Kilian/Results/freq_ 90%Inv .csv"
 # File = "/home/taubier/Documents/SAUVEGARDES_STAGIAIRES/Kilian/Results/freq_ WithoutCrossover .csv"
 # File = "/home/taubier/Documents/SAUVEGARDES_STAGIAIRES/Kilian/Results/freq_ HigherMutRate .csv"
 # File = "/home/taubier/Documents/SAUVEGARDES_STAGIAIRES/Kilian/Results/freq_ WithCrossover .csv"
-# File = "/home/taubier/Documents/SAUVEGARDES_STAGIAIRES/Kilian/Results/freq_ Test_m2 .csv"
 # File = "/home/taubier/Documents/SAUVEGARDES_STAGIAIRES/Kilian/Results/Result_files/freq_ Before_recombination_function .csv"
 # File = "/home/taubier/Documents/SAUVEGARDES_STAGIAIRES/Kilian/Results/Result_files/freq_ After_recombination_function .csv"
 # File = "/home/taubier/Documents/SAUVEGARDES_STAGIAIRES/Kilian/Results/Result_files/freq_ Recoded_recombination_function .csv"
 # File = "/home/taubier/Documents/SAUVEGARDES_STAGIAIRES/Kilian/Results/freq_ N_1000_WithCrossingOver .csv"
 # File = "/home/taubier/Documents/SAUVEGARDES_STAGIAIRES/Kilian/Results/Result_files/freq_ Without_migration .csv"
-File = "/home/taubier/Documents/SAUVEGARDES_STAGIAIRES/Kilian/Results/Result_files/freq_ HigherSelectionMutation .csv"
-
-
+# File = "/home/taubier/Documents/SAUVEGARDES_STAGIAIRES/Kilian/Results/Result_files/freq_ HigherSelectionMutation .csv"
+# File = "/home/taubier/Documents/SAUVEGARDES_STAGIAIRES/Kilian/Results/Result_files/freq_ S_INV = 0.1 .csv"
+# File = "/home/taubier/Documents/SAUVEGARDES_STAGIAIRES/Kilian/Results/Result_files/freq_ S_INV = 0.05 .csv"
+# File = "/home/taubier/Documents/SAUVEGARDES_STAGIAIRES/Kilian/Results/Result_files/freq_ NoInversion Rep 1 .csv"
+# File = "/home/taubier/Documents/SAUVEGARDES_STAGIAIRES/Kilian/Results/Result_files/freq_ MoreTime Rep 1 .csv"
+# File = "/home/taubier/Documents/SAUVEGARDES_STAGIAIRES/Kilian/Results/Result_files/freq_ No Inversion_HigherSelectionMutation (SINV=0.2,Mu=1e-5) Rep 5 .csv"
+# File = "/home/taubier/Documents/SAUVEGARDES_STAGIAIRES/Kilian/Results/Result_files/freq_ No Inversion_HigherSelectionMutation (SINV=0.1,Mu=1e-5) Rep 5 .csv"
+File = "/home/taubier/Documents/SAUVEGARDES_STAGIAIRES/Kilian/Results/Result_files/freq_ Test_fitness Rep 1 .csv"
 
 inversions <- read.csv(File, stringsAsFactors = FALSE)
 
@@ -27,13 +31,20 @@ for (i in 1:length(inversions$n_mut_per_inv_haplosome)) {
 }
 
 inverted_max_mut <- max(as.numeric(inversions$n_mut_per_inv_haplosome))
+inverted_max_fitness <- max(inversions$inv_fitness)
 non_inverted_max_mut <- max(as.numeric(inversions$n_mut_per_non_inv_haplosome))
+non_inverted_max_fitness <- max(inversions$non_inv_fitness)
 max = max(inverted_max_mut,non_inverted_max_mut)
+max_fitness = max(inverted_max_fitness,non_inverted_max_fitness)
 
-par(mfrow = c(1,2))
+par(mfrow = c(1,3))
 plot(x = inversions$tick, y = inversions$inv_freq, col = "red", type = "l", xlab = "Generations", ylab = "Frequency of the inversion", main = "Inversion frequency evolution",xlim = c(0,200000))
 plot(x = inversions$tick, y = inversions$n_mut_per_inv_haplosome, col = "black", type = "l", xlab = "Generations", ylab = "Number of mutations in the inversion area", main = "Evolution of the number of mutations in the inversion area",xlim = c(0,200000),ylim = c(0,max*1.2))
 lines(x = inversions$tick, y = inversions$n_mut_per_non_inv_haplosome, col = "blue", type = "l")
+legend("topleft", legend=c("Non-inverted haplosome", "Inverted haplosome"),
+       col=c("blue", "black"), lty=1:2, cex=0.8)
+plot(x = inversions$tick, y = inversions$inv_fitness, col = "black", type = "l", xlab = "Generations", ylab = "Marginal fitness of haplosomes", main = "Evolution of the marginal fitness of haplosomes",xlim = c(0,200000),ylim = c(1,max_fitness+0.005))
+lines(x = inversions$tick, y = inversions$non_inv_fitness, col = "blue", type = "l")
 legend("topleft", legend=c("Non-inverted haplosome", "Inverted haplosome"),
        col=c("blue", "black"), lty=1:2, cex=0.8)
 
